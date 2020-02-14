52a8249eea35f6d9d82bcfd82e15ea3f4466e203
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-02-15 02:49:30 +0530

    added videos and completed project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Navbar from './src/components/Navbar'
+import Tabbar from './src/components/Tabbar'
+import Video from './src/components/Video'
+import data from './src/data/search.json'
 
 export default function App() {
-  return (
+
+  return ( 
     <View style={styles.container}>
       <Navbar />
+      <ScrollView style={{flex: 1}}>
+        <Video video={data.items[0]} />
+        <Video video={data.items[1]} />
+        <Video video={data.items[2]} />
+        <Video video={data.items[3]} />
+        <Video video={data.items[4]} />
+      </ScrollView>
+      <Tabbar />
     </View>
   );
 }
